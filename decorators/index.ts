1a3c3de5366b49aff48f5f5195d2c4385d4ee1ed
kuku <wuzhao.li@thoughtworks.com>
2021-07-27 13:13:59 +0800

    mupi-release: 0.0.2-alpha.17

--- decorators/index.ts
+++ decorators/index.ts
@@ -1,4 +1,5 @@
 export * from './id.decorator';
-export * from './upload.decorator';
-export * from './description.decorator';
 export * from './title.decorator';
+export * from './description.decorator';
+export * from './upload.decorator';
+export * from './list.decorator';
